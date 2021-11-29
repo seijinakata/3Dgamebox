7dd0f1cbc5729b433867866b6a9f527b690c5947
seijinakata <50951461+seijinakata@users.noreply.github.com>
2021-11-29 22:12:54 +0900

    Add files via upload

---
+++ paint.mjs
@@ -0,0 +1,99 @@
+export class pixel{	
+  constructor(){
+    this.x = -1;
+    this.y = -1;
+    this.z = -1;
+    this.r = -1;
+    this.g = -1;
+    this.b = -1;
+    this.a = -1;
+  }
+  setPixel(x,y,z,r,g,b,a){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+  getPixel(){
+  	let getPixel = [];
+  	getPixel.x = this.x;
+  	getPixel.y = this.y;
+  	getPixel.z = this.z;
+  	getPixel.r = this.r;
+  	getPixel.g = this.g;
+  	getPixel.b = this.b;
+  	getPixel.a = this.a;
+  	return getPixel;
+  }
+}
+
+export function dotPaint(x,y,r,g,b,a,ctx){
+	//バッククオート
+	ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
+	ctx.fillRect(x,y,1,1);
+}
+export function dotLinePaint(Xs,Xe,Ys,Ye,r,g,b,a,ctx){
+	if(Math.abs(Xe-Xs)>=Math.abs(Ye-Ys)){
+	let lineStartx = 0;
+	let lineEndx = 0;
+		if(Xe>=Xs){
+			lineStartx = Xs;
+			lineEndx = Xe;
+		}else{
+			lineStartx = Xe;
+			lineEndx = Xs;
+		}
+		for (let X = lineStartx; X <= lineEndx; X++) {
+			let Y = Math.round(((Ye-Ys)/(Xe-Xs))*(X-Xs)+Ys);
+			dotPaint(X,Y,r,g,b,a,ctx);
+		}
+	}else{
+	let lineStarty = 0;
+	let lineEndy = 0;
+		if(Ye>=Ys){
+			lineStarty = Ys;
+			lineEndy = Ye;
+		}else{
+			lineStarty = Ye;
+			lineEndy = Ys;
+		}
+		for (let Y = lineStarty; Y <= lineEndy; Y++) {
+			let X = Math.round(((Xe-Xs)/(Ye-Ys))*(Y-Ys)+Xs);
+			dotPaint(X,Y,r,g,b,a,ctx);
+		}
+	}
+}
+export function dotLineBufferRegister(Xs,Xe,Ys,Ye,r,g,b,a,buffer){
+	if(Math.abs(Xe-Xs)>=Math.abs(Ye-Ys)){
+	let lineStartx = 0;
+	let lineEndx = 0;
+		if(Xe>=Xs){
+			lineStartx = Xs;
+			lineEndx = Xe;
+		}else{
+			lineStartx = Xe;
+			lineEndx = Xs;
+		}
+		for (let X = lineStartx; X <= lineEndx; X++) {
+			let Y = Math.round(((Ye-Ys)/(Xe-Xs))*(X-Xs)+Ys);
+			buffer[X][Y].setPixel(X,Y,1,r,g,b,a);
+		}
+	}else{
+	let lineStarty = 0;
+	let lineEndy = 0;
+		if(Ye>=Ys){
+			lineStarty = Ys;
+			lineEndy = Ye;
+		}else{
+			lineStarty = Ye;
+			lineEndy = Ys;
+		}
+		for (let Y = lineStarty; Y <= lineEndy; Y++) {
+			let X = Math.round(((Xe-Xs)/(Ye-Ys))*(Y-Ys)+Xs);
+			buffer[X][Y].setPixel(X,Y,1,r,g,b,a);
+		}
+	}
+}
